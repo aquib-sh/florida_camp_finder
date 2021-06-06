46e7bf850e5db0a2e950d9eeb365c85fb3c876c3
Aquib <shaikhaquib394@gmail.com>
2021-06-06 14:31:04 +0530

    Adds configuration file

---
+++ config.py
@@ -0,0 +1,10 @@
+#Author : Shaikh Aquib
+#Date   : June 2021
+
+#=========================-------------------------------=============================
+#=========================| FLORIDA CAMPS CONFIGURATION FILE |========================
+#=========================-------------------------------=============================
+
+website = "https://reserve.floridastateparks.org/Web/Default.aspx"
+browser = "Firefox"
+
