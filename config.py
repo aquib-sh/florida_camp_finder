c120746b6811d7bf80cf22406e7f88fe4a1949dd
Aquib <shaikhaquib394@gmail.com>
2021-06-09 16:51:55 +0530

    Adds configuration file for bot.

--- config.py
+++ config.py
@@ -5,6 +5,10 @@
 #=========================| FLORIDA CAMPS CONFIGURATION FILE |========================
 #=========================-------------------------------=============================
 
-website = "https://reserve.floridastateparks.org/Web/Default.aspx"
+website = "https://reserve.floridastateparks.org/Web/Facilities/SearchViewUnitAvailabity.aspx"
+website_load_time = 15
+runtime_interval_mins = 5
 browser = "Firefox"
 
+input_file = "data/Campsite Input.csv"
+token = ""
